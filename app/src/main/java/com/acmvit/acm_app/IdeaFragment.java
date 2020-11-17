3092439a116e76a8bc124584c7b5a600eb674210
Dinesh B S <dineshbs444@gmail.com>
2020-11-17 17:28:17 +0530

    Renamed idea_fragment.xml to fragment_dashboard.xml and some minor changes!

--- app/src/main/java/com/acmvit/acm_app/IdeaFragment.java
+++ app/src/main/java/com/acmvit/acm_app/IdeaFragment.java
@@ -23,7 +23,7 @@ public class IdeaFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.idea_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 
     @Override
