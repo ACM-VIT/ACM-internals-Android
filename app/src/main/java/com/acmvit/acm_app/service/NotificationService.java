1ee28f8e4104764fc04e3b3c150a605eacf4eede
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-20 18:49:42 +0530

    feat(notification): Configure to recieve messages

---
+++ app/src/main/java/com/acmvit/acm_app/service/NotificationService.java
@@ -0,0 +1,80 @@
+package com.acmvit.acm_app.service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.acmvit.acm_app.MainActivity;
+import com.acmvit.acm_app.R;
+import com.acmvit.acm_app.util.Constants;
+import com.acmvit.acm_app.util.GeneralUtils;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class NotificationService extends FirebaseMessagingService {
+    private static final String TAG = "NotificationService";
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        //TODO: Send Token to the backend
+        super.onNewToken(s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Map<String, String> data = remoteMessage.getData();
+            Log.d(TAG, "Message data payload: " + data);
+
+            String title = data.get(Constants.ProjectNotification.MSG_TITLE);
+            String body = data.get(Constants.ProjectNotification.MSG_BODY);
+            NotificationCompat.Builder builder = buildNotification(title, body);
+            sendNotification(builder.build());
+        }
+
+    }
+
+    private NotificationCompat.Builder buildNotification(String title, String body){
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        return new NotificationCompat.Builder(this, Constants.ProjectNotification.CHANNEL_ID)
+                .setContentIntent(resultPendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(body))
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+    }
+
+    private void sendNotification(Notification notification){
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    Constants.ProjectNotification.CHANNEL_ID,
+                    Constants.ProjectNotification.CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription(Constants.ProjectNotification.CHANNEL_DISP);
+            notificationManager.createNotificationChannel(channel);
+        }
+        notificationManager.notify(GeneralUtils.generateUniqueId(), notification);
+    }
+
+}
---
+++ app/src/main/java/com/acmvit/acm_app/util/Constants.java
@@ -0,0 +1,13 @@
+package com.acmvit.acm_app.util;
+
+public class Constants {
+    public static class ProjectNotification{
+        public static final String MSG_TITLE = "title";
+        public static final String MSG_BODY = "body";
+        public static final String CHANNEL_ID = "PROJECTS_CHANNEL";
+        public static final String CHANNEL_NAME = "projects";
+        public static final String CHANNEL_DISP = "You get notified when your teammates create new projects";
+    }
+
+
+}
