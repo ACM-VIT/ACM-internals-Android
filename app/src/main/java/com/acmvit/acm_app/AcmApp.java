c20f5ccd187ccb14f8320a8dd180572d251217e7
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-20 17:27:52 +0530

    Add Application class

---
+++ app/src/main/java/com/acmvit/acm_app/AcmApp.java
@@ -0,0 +1,11 @@
+package com.acmvit.acm_app;
+
+import android.app.Application;
+
+public class AcmApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
