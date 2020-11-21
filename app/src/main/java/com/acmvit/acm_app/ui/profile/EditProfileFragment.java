7763f9107aa05fe9b529c5c91e30f406d37fe17c
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-21 22:34:13 +0530

    refactor(user): Rename AccountsIntegrationFragment to EditProfileFragment

--- app/src/main/java/com/acmvit/acm_app/ui/profile/EditProfileFragment.java
+++ app/src/main/java/com/acmvit/acm_app/ui/profile/EditProfileFragment.java
@@ -1,4 +1,4 @@
-package com.acmvit.acm_app.ui.main;
+package com.acmvit.acm_app.ui.profile;
 
 import android.os.Bundle;
 
@@ -12,10 +12,10 @@ import com.acmvit.acm_app.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AccountsIntegrationFragment#newInstance} factory method to
+ * Use the {@link EditProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AccountsIntegrationFragment extends Fragment {
+public class EditProfileFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class AccountsIntegrationFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public AccountsIntegrationFragment() {
+    public EditProfileFragment() {
         // Required empty public constructor
     }
 
@@ -39,8 +39,8 @@ public class AccountsIntegrationFragment extends Fragment {
      * @return A new instance of fragment AccountsIntegrationFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AccountsIntegrationFragment newInstance(String param1, String param2) {
-        AccountsIntegrationFragment fragment = new AccountsIntegrationFragment();
+    public static EditProfileFragment newInstance(String param1, String param2) {
+        EditProfileFragment fragment = new EditProfileFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class AccountsIntegrationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_accounts_integration, container, false);
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
     }
 }
