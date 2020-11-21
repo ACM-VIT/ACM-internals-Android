841f0956164301cc34c8a47e59b63f0e7ce522d9
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-21 14:16:22 +0530

    feat(core): Configure base viewmodel

---
+++ app/src/main/java/com/acmvit/acm_app/ui/base/BaseViewModel.java
@@ -0,0 +1,16 @@
+package com.acmvit.acm_app.ui.base;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.acmvit.acm_app.AcmApp;
+
+public class BaseViewModel extends ViewModel {
+    protected final ActivityViewModel activityViewModel;
+
+    public BaseViewModel(ActivityViewModel activityViewModel) {
+        this.activityViewModel = activityViewModel;
+    }
+}
