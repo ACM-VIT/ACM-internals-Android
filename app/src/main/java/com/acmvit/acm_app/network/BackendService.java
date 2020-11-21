6c8262b13f3a70ae953b09f8b1f6efcd18e237c0
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-21 16:03:04 +0530

    feat(auth): Configure google access token endpoint

---
+++ app/src/main/java/com/acmvit/acm_app/network/BackendService.java
@@ -0,0 +1,20 @@
+package com.acmvit.acm_app.network;
+
+
+import com.acmvit.acm_app.model.AuthData;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface BackendService {
+    //Get Google Access Token
+    @POST("/App/v1/access/login/google")
+    Call<BackendResponse<AuthData>> getAccessToken(@Header("Authorization") String token);
+
+
+}
