5d1e9620bd07567936883486b725205aeed9f86e
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-21 16:08:35 +0530

    feat(core): Configure backend constants

--- app/src/main/java/com/acmvit/acm_app/util/Constants.java
+++ app/src/main/java/com/acmvit/acm_app/util/Constants.java
@@ -9,5 +9,10 @@ public class Constants {
         public static final String CHANNEL_DISP = "You get notified when your teammates create new projects";
     }
 
+    public static class Backend {
+        public static String BASE_URL = "https://us-central1-acminternal.cloudfunctions.net";
+        public static String SUCCESS_STATUS = "10000";
+        public static int TOKEN_RETRY_COUNT = 2;
+    }
 
 }
