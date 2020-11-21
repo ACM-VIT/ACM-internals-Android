5e6497a331354267853e6f4f6d6d229715dcb4c3
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-21 15:55:48 +0530

    feat(core): Add network result classes

---
+++ app/src/main/java/com/acmvit/acm_app/util/Resource.java
@@ -0,0 +1,41 @@
+package com.acmvit.acm_app.util;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import static com.acmvit.acm_app.util.Status.ERROR;
+import static com.acmvit.acm_app.util.Status.LOADING;
+import static com.acmvit.acm_app.util.Status.SUCCESS;
+
+public class Resource<T> {
+    @NonNull
+    public final Status status;
+
+    @Nullable
+    public final T data;
+
+    @Nullable private final String message;
+
+    private Resource(@NonNull Status status, @Nullable T data, @Nullable String message) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
+
+    public static <T> Resource<T> success(@NonNull T data) {
+        return new Resource<>(SUCCESS, data, null);
+    }
+
+    public static <T> Resource<T> error(String msg, @Nullable T data) {
+        return new Resource<>(ERROR, data, msg);
+    }
+
+    public static <T> Resource<T> loading(@Nullable T data) {
+        return new Resource<>(LOADING, data, null);
+    }
+
+    @Nullable
+    public String getMessage() {
+        return message;
+    }
+}
