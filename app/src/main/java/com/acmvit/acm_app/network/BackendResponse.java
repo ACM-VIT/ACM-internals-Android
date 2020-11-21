04b926b62e519e9a6006372b500bcaa3cb1704f2
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-21 15:58:45 +0530

    feat(core): Add backend response generic class

---
+++ app/src/main/java/com/acmvit/acm_app/network/BackendResponse.java
@@ -0,0 +1,54 @@
+package com.acmvit.acm_app.network;
+
+import com.google.gson.annotations.SerializedName;
+
+public class BackendResponse<T> {
+
+    @SerializedName("statusCode")
+    private String statusCode;
+
+    @SerializedName("message")
+    private String message;
+
+    @SerializedName("data")
+    private T data;
+
+    public BackendResponse(String statusCode, String message, T data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthResponse{" +
+                "statusCode='" + statusCode + '\'' +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
