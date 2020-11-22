3dddcf1ba9d11efadb64e0f4672edbd1f81c152f
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-22 16:30:09 +0530

    feat(user): Configure discord integration on view side

--- app/src/main/java/com/acmvit/acm_app/ui/profile/EditProfileFragment.java
+++ app/src/main/java/com/acmvit/acm_app/ui/profile/EditProfileFragment.java
@@ -1,66 +1,84 @@
 package com.acmvit.acm_app.ui.profile;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.acmvit.acm_app.BaseViewModelFactory;
 import com.acmvit.acm_app.R;
+import com.acmvit.acm_app.databinding.FragmentEditProfileBinding;
+import com.acmvit.acm_app.service.AuthService;
+import com.acmvit.acm_app.util.Constants;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import net.openid.appauth.AuthorizationException;
+import net.openid.appauth.AuthorizationRequest;
+import net.openid.appauth.AuthorizationResponse;
+import net.openid.appauth.AuthorizationService;
+import net.openid.appauth.AuthorizationServiceConfiguration;
+import net.openid.appauth.ResponseTypeValues;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
 public class EditProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentEditProfileBinding binding;
+    private static final int RC_LOGIN = 101;
+    private AuthorizationServiceConfiguration authServiceConfig;
+    private AuthService authService;
+    private EditProfileViewModel viewModel;
 
     public EditProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountsIntegrationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditProfileFragment newInstance(String param1, String param2) {
-        EditProfileFragment fragment = new EditProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+
+        binding = FragmentEditProfileBinding.inflate(inflater, container, false);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        authService = AuthService.getInstance();
+
+        //Init Viewmodel
+        BaseViewModelFactory factory = new BaseViewModelFactory(getActivity());
+        viewModel = new ViewModelProvider(this, factory).get(EditProfileViewModel.class);
+
+        initObservers();
+    }
+
+    private void initObservers() {
+        viewModel.getStartResultActivity().observe(getViewLifecycleOwner(), intent -> {
+            startActivityForResult(intent, RC_LOGIN);
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_LOGIN){
+            viewModel.sendTokenFromIntent(data);
+        }
     }
 }
