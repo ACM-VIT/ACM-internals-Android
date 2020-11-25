bbf47711584dd890d4bcaa0e1eb1ae7983548529
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-25 21:43:37 +0530

    refactor(splash): Move splash delay logic to VM

--- app/src/main/java/com/acmvit/acm_app/ui/splash/SplashViewModel.java
+++ app/src/main/java/com/acmvit/acm_app/ui/splash/SplashViewModel.java
@@ -1,11 +1,13 @@
 package com.acmvit.acm_app.ui.splash;
 
 import android.app.Application;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
-
 import com.acmvit.acm_app.AcmApp;
 import com.acmvit.acm_app.model.UserData;
 import com.acmvit.acm_app.pref.SessionManager;
@@ -20,9 +22,11 @@ import com.acmvit.acm_app.util.Status;
 
 public class SplashViewModel extends AndroidViewModel {
     private static final String TAG = "SplashViewModel";
+    private static final int SPLASH_TIMEOUT = 2500;
     private final SessionManager sessionManager;
     private final UserRepository userRepository;
     private final Application application;
+    private final MutableLiveData<Boolean> canNavigate = new MutableLiveData<>(false);
 
     public SplashViewModel(Application application) {
         super(application);
@@ -31,10 +35,18 @@ public class SplashViewModel extends AndroidViewModel {
         this.application = application;
     }
 
+    public void startNavigationIntent(){
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> canNavigate.setValue(true), SPLASH_TIMEOUT);
+    }
+
     public void fetchUserDetails(){
         if(sessionManager.getAuthState()){
             userRepository.sendFCMTokenUsingWM(application);
         }
     }
 
+    public LiveData<Boolean> getCanNavigate() {
+        return canNavigate;
+    }
 }
