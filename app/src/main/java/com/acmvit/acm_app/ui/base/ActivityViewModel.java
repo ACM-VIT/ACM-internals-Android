23f9bf7be827aa52ff12f4c8c6b8ca588162a1cd
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-25 10:52:26 +0530

    feat(core): Configure UI communication for one time events

--- app/src/main/java/com/acmvit/acm_app/ui/base/ActivityViewModel.java
+++ app/src/main/java/com/acmvit/acm_app/ui/base/ActivityViewModel.java
@@ -1,25 +1,32 @@
 package com.acmvit.acm_app.ui.base;
 
-import android.view.View;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.acmvit.acm_app.AcmApp;
+import com.acmvit.acm_app.model.UserData;
 import com.acmvit.acm_app.pref.SessionManager;
-import com.acmvit.acm_app.ui.auth.LoginViewModel;
+import com.acmvit.acm_app.repository.UserRepository;
+import com.acmvit.acm_app.util.Action;
+import com.acmvit.acm_app.util.Resource;
+import com.acmvit.acm_app.util.SingleLiveEvent;
 
 public class ActivityViewModel extends ViewModel {
 
     private final SessionManager sessionManager = AcmApp.getSessionManager();
     private final LiveData<Boolean> loginState = sessionManager.getAuthStateNotifier();
     private final LiveData<Boolean> networkState = AcmApp.getIsConnected();
-    private final MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> isLoading = new MutableLiveData<>(false);
+    private final SingleLiveEvent<Action> action = new SingleLiveEvent<>();
 
     public boolean canRunNetworkTask(){
-        return networkState.getValue() && !isLoading.getValue();
+        return networkState.getValue();
+    }
+
+    public boolean canRunAuthenticatedNetworkTask(){
+        return canRunNetworkTask() && sessionManager.getAuthState();
     }
 
     public LiveData<Boolean> getLoginState() {
@@ -37,4 +44,13 @@ public class ActivityViewModel extends ViewModel {
     public void setIsLoading(boolean isLoading) {
         this.isLoading.setValue(isLoading);
     }
+
+    public LiveData<Action> getAction() {
+        return action;
+    }
+
+    public void setAction(Action action){
+        this.action.setValue(action);
+    }
+
 }
