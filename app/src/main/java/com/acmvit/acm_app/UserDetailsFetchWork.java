6216bb96fc60a254d71915a84c0fea5a3e09b2c3
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-25 10:49:23 +0530

    feat(user): Add WM to fetch user on startup

---
+++ app/src/main/java/com/acmvit/acm_app/UserDetailsFetchWork.java
@@ -0,0 +1,58 @@
+package com.acmvit.acm_app;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.ListenableWorker;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.acmvit.acm_app.repository.UserRepository;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public class UserDetailsFetchWork extends Worker {
+    private final UserRepository userRepository;
+
+    public UserDetailsFetchWork(
+            @NonNull Context context,
+            @NonNull WorkerParameters params) {
+        super(context, params);
+        userRepository = UserRepository.getInstance();
+    }
+
+    @NotNull
+    @Override
+    public ListenableWorker.Result doWork() {
+        boolean status = false;
+        try {
+            status = userRepository.fetchUser();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return status ? ListenableWorker.Result.success() : ListenableWorker.Result.retry();
+    }
+
+    public static void fetchUserUsingWM(Context context) {
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+
+        WorkRequest sendTokenWork =
+                new OneTimeWorkRequest.Builder(SendFCMTokenWork.class)
+                        .setConstraints(constraints)
+                        .build();
+
+        WorkManager.getInstance(context).enqueue(sendTokenWork);
+    }
+
+}
