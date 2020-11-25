c63b9714370cf4d2a50795563b691a32b5b13fbf
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-25 10:49:59 +0530

    refactor(user): Change redirect url

--- app/src/main/java/com/acmvit/acm_app/util/Constants.java
+++ app/src/main/java/com/acmvit/acm_app/util/Constants.java
@@ -17,10 +17,11 @@ public class Constants {
         public static final String BASE_URL = "https://us-central1-acminternal.cloudfunctions.net";
         public static final String SUCCESS_STATUS = "10000";
         public static final int TOKEN_RETRY_COUNT = 2;
+        public static final String DP_STORAGE_LOC = "profileDP";
     }
 
     public static class Discord{
-        public static final String REDIRECT_URL = "http://localhost";
+        public static final String REDIRECT_URL = "https://auth.expo.io/@madrigal1/acminternalapprn";
         public static final String AUTH_URL = "https://discord.com/api/oauth2/authorize";
         public static final String TOKEN_URL = "https://discord.com/api/oauth2/token";
         public static final String[] SCOPES = {"identify", "email"};
