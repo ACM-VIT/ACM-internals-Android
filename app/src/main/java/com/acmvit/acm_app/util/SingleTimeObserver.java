3879f13505ad13a0479030de76088330aa161b39
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-25 10:51:33 +0530

    feat(core): Add custom Livedata classes

---
+++ app/src/main/java/com/acmvit/acm_app/util/SingleTimeObserver.java
@@ -0,0 +1,36 @@
+package com.acmvit.acm_app.util;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+public abstract class SingleTimeObserver<T> implements Observer<T> {
+    private LiveData<T> liveData;
+
+    public void attachTo(@NonNull LiveData<T> liveData){
+        detachLiveData();
+        liveData.observeForever(this);
+    }
+
+    public void attachTo(LifecycleOwner owner, @NonNull LiveData<T> liveData){
+        detachLiveData();
+        liveData.observe(owner, this);
+    }
+
+    public void detachLiveData(){
+        if(this.liveData != null){
+            this.liveData.removeObserver(this);
+        }
+    }
+
+    @Override
+    public void onChanged(T t) {
+        detachLiveData();
+        onReceived(t);
+    }
+
+    public abstract void onReceived(T t);
+
+}
