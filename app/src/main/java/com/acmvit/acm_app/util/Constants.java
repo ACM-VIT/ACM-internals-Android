d7ab89207f180390979aafbc8ae5514a03a6c03b
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-11-27 00:01:36 +0530

    refactor(notification): Change body key to desc

--- app/src/main/java/com/acmvit/acm_app/util/Constants.java
+++ app/src/main/java/com/acmvit/acm_app/util/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static class ProjectNotification{
         public static final String TOPIC = "projects";
         public static final String MSG_TITLE = "title";
-        public static final String MSG_BODY = "body";
+        public static final String MSG_BODY = "desc";
         public static final String CHANNEL_ID = "PROJECTS_CHANNEL";
         public static final String CHANNEL_NAME = "projects";
         public static final String CHANNEL_DISP = "You get notified when your teammates create new projects";
