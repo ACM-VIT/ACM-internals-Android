6d4fa898c7e0d94e14e79192992a1f11c97b3f53
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-12-30 22:37:24 +0530

    feat(events): Configure SwipeHelper and Viewpager

---
+++ app/src/main/java/com/acmvit/acm_app/ui/invites/adapters/EventsPageAdapter.java
@@ -0,0 +1,31 @@
+package com.acmvit.acm_app.ui.invites.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.acmvit.acm_app.ui.invites.NotificationFragment;
+import com.acmvit.acm_app.ui.invites.RequestsFragment;
+
+public class EventsPageAdapter extends FragmentStateAdapter {
+    private static final int SIZE = 2;
+
+    public EventsPageAdapter(@NonNull Fragment fragment) {
+        super(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        if(position == 0){
+            return new NotificationFragment();
+        }
+        return new RequestsFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return SIZE;
+    }
+}
