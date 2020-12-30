87190196a9484c406b7fb7bd505ce231d9d4b99c
Amit Krishna A <amit.ananthkumar@gmail.com>
2020-12-30 22:35:09 +0530

    feat(events): Configure Fragments, Adapters, Viewpager

---
+++ app/src/main/java/com/acmvit/acm_app/ui/invites/RequestsFragment.java
@@ -0,0 +1,45 @@
+package com.acmvit.acm_app.ui.invites;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+
+import com.acmvit.acm_app.databinding.FragmentRequestsBinding;
+import com.acmvit.acm_app.model.Request;
+import com.acmvit.acm_app.ui.invites.adapters.RequestsRvAdapter;
+import com.acmvit.acm_app.ui.invites.adapters.RequestsSwipeHelper;
+import com.acmvit.acm_app.util.MockData;
+
+import java.util.List;
+
+public class RequestsFragment extends Fragment {
+    private FragmentRequestsBinding binding;
+    private List<Request> requests;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentRequestsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        requests = MockData.requests;
+        initRv();
+    }
+
+    private void initRv(){
+        RequestsRvAdapter adapter = new RequestsRvAdapter(requests);
+        binding.requestsRv.setAdapter(adapter);
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RequestsSwipeHelper(adapter));
+        itemTouchHelper.attachToRecyclerView(binding.requestsRv);
+    }
+}
