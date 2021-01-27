6679cc7d736e72cb51177e2a9fbcf12aa8954755
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-01-27 13:32:46 +0530

    feat(profile): Improve accounts view UI

---
+++ app/src/main/java/com/acmvit/acm_app/ui/profile/CLIPasswordDialog.java
@@ -0,0 +1,81 @@
+package com.acmvit.acm_app.ui.profile;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.acmvit.acm_app.ui.base.BaseViewModelFactory;
+import com.acmvit.acm_app.databinding.DialogCliPasswordBinding;
+import com.acmvit.acm_app.ui.base.BaseActivity;
+
+public class CLIPasswordDialog extends DialogFragment {
+    public static final String TAG = "CLIPasswordDialog";
+    private DialogCliPasswordBinding binding;
+    private CLIPasswordViewModel viewModel;
+    private OnDismissListener onDismissListener = () -> {
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        binding = DialogCliPasswordBinding.inflate(inflater, container, false);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        binding.inputPwd.getEditText().clearFocus();
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Init ViewModel
+        BaseViewModelFactory factory = new BaseViewModelFactory(
+                (BaseActivity) getActivity()
+        );
+        viewModel = new ViewModelProvider(this, factory).get(CLIPasswordViewModel.class);
+
+        binding.setViewmodel(viewModel);
+        initObservers();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Window dialogWindow = getDialog().getWindow();
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.copyFrom(getDialog().getWindow().getAttributes());
+        layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialogWindow.setAttributes(layoutParams);
+        dialogWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+    }
+
+    @Override
+    public void onDismiss(@NonNull DialogInterface dialog) {
+        super.onDismiss(dialog);
+        onDismissListener.onDismiss();
+    }
+
+    private void initObservers() {
+        viewModel.getDismissDialogLiveData().observe(getViewLifecycleOwner(), trigger -> dismiss());
+    }
+
+    public void setOnDismissListener(OnDismissListener onDismissListener) {
+        this.onDismissListener = onDismissListener;
+    }
+
+    public interface OnDismissListener {
+        void onDismiss();
+    }
+}
