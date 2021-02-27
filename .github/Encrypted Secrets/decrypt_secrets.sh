33d8a2be8e7c2870ee98be1761a4303dd30e0b5e
Amit Krishna A <54491362+akri16@users.noreply.github.com>
2021-02-27 11:18:02 +0530

    Update decrypt_secrets.sh

--- .github/Encrypted Secrets/decrypt_secrets.sh
+++ .github/Encrypted Secrets/decrypt_secrets.sh
@@ -3,7 +3,7 @@
 # Decrypt the files
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_DECRYPT_PASSPHRASE" \
---output /app/google-services.json google-services.json.gpg
+--output /app/google-services.json .github/Encrypted\ Secrets/google-services.json.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_DECRYPT_PASSPHRASE" \
---output /src/main/res/values/secrets.xml secrets.xml.gpg
+--output /src/main/res/values/secrets.xml .github/Encrypted\ Secrets/secrets.xml.gpg
