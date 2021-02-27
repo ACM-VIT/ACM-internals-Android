e63b6f657e8a8c49e8137d18aaea565b0daab71a
Amit Krishna A <54491362+akri16@users.noreply.github.com>
2021-02-27 11:29:01 +0530

    chore(ci): Create build workflow
    * chore(ci): Create build workflow
    
    * chore(ci): Add encrypted secrets
    
    * chore(ci): Add decrypt_secrets script
    
    * chore(ci): Add decrypt secrets cmd to build.yml
    
    * Update build.yml
    
    * Update build.yml
    
    * Update decrypt_secrets.sh encoding
    
    * Update build.yml
    
    * Update build.yml
    
    * Update decrypt_secrets.sh
    
    * Update decrypt_secrets.sh
    
    * Update decrypt_secrets.sh
    
    * Update decrypt_secrets.sh

---
+++ .github/Encrypted Secrets/decrypt_secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Decrypt the files
+
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_DECRYPT_PASSPHRASE" \
+--output app/google-services.json .github/Encrypted\ Secrets/google-services.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_DECRYPT_PASSPHRASE" \
+--output app/src/main/res/values/secrets.xml .github/Encrypted\ Secrets/secrets.xml.gpg
